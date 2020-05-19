916e95f92513e9eb6def704f338b792c80d5abdc
kureharyosuke <ohhayo.com@gmail.com>
2020-05-20 05:06:09 +0900

    first commit

---
+++ models/subscriber.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const subscriberSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  subscribedChannel: {
+    type: String,
+    required: true
+  },
+  subscribeDate: {
+    type: Date,
+    required: true,
+    default: Date.now
+  }
+})
+
+// exporting out subscriber schema
+
+module.exports = mongoose.model('Subscriber', subscriberSchema)
